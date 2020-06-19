420be70ef98f68b0333f642a62c96184c6387edf
Yun Sum (Edmund) Wong <edmund.wong@live.ca>
2020-06-19 09:31:44 -0400

    Added POST/DELETE functionality

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const notesData = require('./db/db.json')
+let notesData = require('./db/db.json')
 const express = require('express')
 const fs = require('fs')
 
@@ -7,6 +7,7 @@ const PORT = 8080;
 
 app.use(express.json())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
 
 
 // GET `/notes` - Should return the `notes.html` file.
@@ -14,18 +15,29 @@ app.get('/notes',function(req,res) {
     res.redirect('/notes.html')
 })
 
+
 // GET `/api/notes` - Should read the `db.json` file and return all saved notes as JSON.
-app.get('/api/notes', function(re,res){
+app.get('/api/notes', function(req,res){
+    for (const [id,obj] of notesData.entries()){
+        obj.id = id+1
+    }
     res.send(notesData)
 })
 
-// POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
-app.post('/api/notes', function(re,res){
-
-})
 
 // DELETE `/api/notes/:id` - Should receive a query parameter containing the id of a note to delete. 
-app.delete('/api/notes/:id', function(re,res){
+app.delete('/api/notes/:id', function(req,res){
+    notesData.splice(req.params.id , 1)
+    fs.writeFileSync('./db/db.json', JSON.stringify(notesData))
+    res.send(notesData)
+})
+
+
+// POST `/api/notes` - Should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client.
+app.post('/api/notes', function(req,res){
+    notesData.push(req.body)
+    fs.writeFileSync('./db/db.json', JSON.stringify(notesData))
+    res.send(notesData)
 
 })
 
@@ -34,9 +46,6 @@ app.delete('/api/notes/:id', function(re,res){
 app.get('*',function(req,res) {
     res.redirect('/')
 })
-
-
-
 
 
 app.listen(PORT, function() {
